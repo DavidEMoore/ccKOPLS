7e6e95f629185f60d19c3e04a54bce034483dc30
David Moore <Dave@Davids-MacBook-Air-3.local>
2015-03-30 08:29:10 -0400

    Updated Parameter_Setting.R

--- Parameter_Setting.R
+++ Parameter_Setting.R
@@ -15,67 +15,61 @@ X.test <- X[-idx,]
 # choose best C
 
 K <- t(X)*X
-kcauc <- matrix(0, nrow = length(CRange), ncol = kfold)
+kcauc <- matrix(0,nrow=CRange,ncol=kfold)
 
 for (i in 1:length(CRange)){
-  c <- CRange[i]
+  c <- CRange[i]  
+  for (j in 1:kfold){
+    test <- which(X.test[j] != 0)
+    train <- which(X.train[j] != 0)
+    ksvm.obj <- ksvm(Class~.,data=X[train],C=c,cross=kfold)
+    labels <- predict(ksvm.obj,test,type="response")
+    kcauc[i,j] <- auc(accuracy(X[test]$Class,response))
+  }
+}
+
+max <- -100
+
+for(i in 1:nrow(kcauc)){
+  a <- max(mean(kcauc[i,]),max)
+}
+
+C <- CRange[a]
+
+#Choose best lambda
+
+for (i in 1:length(LambdaRange)){
+  lam <- LambdaRange(i)
+  
+  rescaled <- Rescaling(X,L,lam)
+  X.new <- rescaled[1]
+  K.new <- rescaled[2]
+  l <- rescaled[3]
+  
+  idx <- round(runif(round(.6*nrow(X.new)), 1, nrow(X.new)))
+  X.train <- X.new[idx,]
+  X.test <- X.new[-idx,]
+  
   for (j in 1:kfold){
     test <- which(X.test[j] != 0)
     train <- which(X.train[j] != 0)
     print (ksvm(X.train,C=c,cross=kfold))
-    kcauc[i,j] <- ksvm(X.train,C=c,cross=kfold)
-  }
+    ksvm.obj <- ksvm(Class~.,data=K.new,C=c,cross=kfold)
+    labels <- predict(ksvm.obj,X.test,type="response")
+    kcauc[i,j] <- auc(accuracy(X.test$Class,response))
+    
+    kauc(i,j) = svm(y,train,test,K.new,C)
+  }   
 }
 
 
-a <- max(mean(kcauc,2))
+max <- -100
 
-C <- CRange[a]
-
-#Choose best lambda
-
-for (i in 1:length(LambdaRange)){
-  lam <- LambdaRange(i)
-
-  rescaled <- Rescaling(X,L,lam)
-  X.new <- rescaled[1]
-  K.new <- rescaled[2]
-  l <- rescaled[3]
-
-  for (j in 1:kfold){
-    test = find(CVO.test(j))
-    train = find(CVO.training(j))
-    kauc(i,j) = svm(y,train,test,K.new,C)
-  }
-  }
-
-
-a <- max(mean(kcauc,2))
-
-C <- CRange[a]
-
-#Choose best lambda
-
-for (i in 1:length(LambdaRange)){
-  lam <- LambdaRange(i)
-
-  rescaled <- Rescaling(X,L,lam)
-  X.new <- rescaled[1]
-  K.new <- rescaled[2]
-  l <- rescaled[3]
-
-  for (j in 1:kfold){
-    test = find(CVO.test(j))
-    train = find(CVO.training(j))
-    kauc(i,j) = svm(y,train,test,K.new,C)
-  }
+for(i in 1:nrow(kcauc)){
+  a <- max(mean(kcauc[i,]),max)
 }
 
-
-a= max(mean(kauc,2))
 lambda = LambdaRange(a)
 
 return (c(lambda,C))
 }
-
-  
