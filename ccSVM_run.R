ce4bf5a5278e3553ac7c61cc96ae61e689bc3b2a
David Moore <dm21953@gmail.com>
2015-06-23 16:16:35 -0400

    fixed index bug in function

--- ccSVM_run.R
+++ ccSVM_run.R
@@ -20,13 +20,13 @@ for(i in 1:length(test.ixs)){
   while(ok == F) {
     tryCatch({
       # ksvm.obj <- ksvm(K.new,y,C=c,kernel='matrix',prob.model=T)
-      ksvm.obj <- ksvm(K.new[-test.ixs,-test.ixs],y[-test.ixs],C=c,kernel='matrix',prob.model=T,type='nu-svc')
+      ksvm.obj <- ksvm(K.new[-test.ixs[[i]],-test.ixs],y[-test.ixs[[i]]],C=c,kernel='matrix',prob.model=T,type='nu-svc')
       # Ktest.new <- as.kernelMatrix(crossprod(t(X.new),t(X.new[SVindex(ksvm.obj), ]))) 
-      Ktest.new <- as.kernelMatrix(crossprod(t(X.new[test.ixs,]),t(X.new[SVindex(ksvm.obj), ])))  
+      Ktest.new <- as.kernelMatrix(crossprod(t(X.new[test.ixs[[i]],]),t(X.new[SVindex(ksvm.obj), ])))  
       # predictions <- predict(ksvm.obj,Ktest.new,type='probabilities')[,2]
       predictions <- predict(ksvm.obj,Ktest.new,type='probabilities')[,2]
       # labels <- y
-      labels <- y[test.ixs]
+      labels <- y[test.ixs[[i]]]
       kcauc <- auc(roc(predictions,labels))
       ok <- T
     },
